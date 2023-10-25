32dae7a42eb95a972f2b8c0c2fc6606bca2aca7c
Ankit Techsopi <ankit.tiwari@techsopi.com>
2023-10-25 12:22:58 +0530

    adding hello world

---
+++ src/main/java/HellloWorld1.java
@@ -0,0 +1,15 @@
+package main.java;
+
+public class HellloWorld1 {
+
+	public static void main(String[] args) {
+		
+		
+		int i = 10;
+		int j  = 12;
+		int k = 100;
+   System.out.println("abc ");
+
+	}
+
+}
